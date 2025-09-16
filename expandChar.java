7b88a248125196f891912a33a390ad7753a94b15
Prasoon Mishra <prasoonmishr.21@gmail.com>
2025-09-16 15:35:00 +0530

    Add files via upload

---
+++ expandChar.java
@@ -0,0 +1,44 @@
+/*
+ This sample code expands characters based on the number that follows them.
+ For example: a2b3 -> aabbb
+ Written by - Prasoon Mishra
+ Dated - 16/09/2025
+*/
+
+import java.util.Scanner;
+
+public class expandChar {
+    public static void main(String[] args) {
+        expand();
+    }
+
+    public static void expand() {
+        Scanner xy = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s = xy.nextLine();
+        xy.close();
+
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+
+        while (i < s.length()) {
+            char currentChar = s.charAt(i);
+
+            if (Character.isLetter(currentChar)) {
+                i++;
+                int count = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    count = count * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                for (int j = 0; j < count; j++) {
+                    result.append(currentChar);
+                }
+            } else {
+                i++;
+            }
+        }
+
+        System.out.println(result.toString());
+    }
+}
