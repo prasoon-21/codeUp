6f112f15dd8396261b03518fc61697dfa684d4d5
Prasoon Mishra <prasoonmishr.21@gmail.com>
2025-09-17 11:37:26 +0530

    Add files via upload

---
+++ Assignment1/subString.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class subString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = scanner.nextLine();
+        scanner.close();
+        String longest = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String sub = s.substring(i, j);
+                boolean hasDuplicate = false;
+                for (int k = 0; k < sub.length(); k++) {
+                    for (int l = k + 1; l < sub.length(); l++) {
+                        if (sub.charAt(k) == sub.charAt(l)) {
+                            hasDuplicate = true;
+                            break;
+                        }
+                    }
+                    if (hasDuplicate) break;
+                }
+                if (!hasDuplicate && sub.length() > longest.length()) {
+                    longest = sub;
+                }
+            }
+        }
+        
+        System.out.println("Longest substring without repeating : " + longest);
+    }
+}
